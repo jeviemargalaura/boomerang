96919e31b79e426efbfa5442d2f968a88790bfc0
jevSushi <jeviemargalaura@gmail.com>
2016-10-26 10:16:02 +0000

    Enable site

--- app/services/configService.js
+++ app/services/configService.js
@@ -2,23 +2,23 @@ angular.module('gdgXBoomerang')
 .factory('Config', function () {
     return {
         // TODO Modify these to configure your app
-        'name'          : 'GDG Space Coast',
-        'id'            : '103959793061819610212',
-        'googleApi'     : 'AIzaSyA9ALjr2iWvhf3Rsz9-bH0cEcDcrdkpuAg',
-        'pwaId'         : '5915725140705884785', // Picasa Web Album id, must belong to Google+ id above
-        'domain'        : 'http://www.gdgspacecoast.org',
-        'twitter'       : 'gdgspacecoast',
-        'facebook'      : 'gdgspacecoast',
-        'youtube'       : 'UCkiYHK3IZMk5XsYZ626b9Rw',
-        'meetup'        : 'gdgspacecoast',
+        'name'          : 'GDG Zamboanga',
+        'id'            : '102615103919428587748',
+        'googleApi'     : 'AIzaSyCRJdlQlS69iIRsjYaKC-AUj2Vii5RfsSA>',
+        'pwaId'         : '', // Picasa Web Album id, must belong to Google+ id above
+        'domain'        : 'http://www.gdgzamboanga.org',
+        'twitter'       : 'gdgzambo',
+        'facebook'      : 'gdgzambo',
+        'youtube'       : 'gdgzambo',
+        'meetup'        : '',
         // Change to 'EEEE, MMMM d, y - H:mm' for 24 hour time format.
         'dateFormat'    : 'EEEE, MMMM d, y - h:mm a',
         'cover' : {
-            title: 'Android Development for Beginners',
-            subtitle: 'Google Developers Study Jams is a free series of global, community-run, in-person study groups.',
+            title: 'Worldwide GDG Events',
+            subtitle: 'Directory of developer events organized by tags and displayed on a global map.',
             button: {
-                text: 'Learn More',
-                url: 'http://developerstudyjams.com/'
+                text: 'Find local events',
+                url: 'http://gdg.events/'
             }
         },
         'activities': {
